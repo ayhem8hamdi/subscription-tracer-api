4dbb3e6afc1d7f7d0c31c537820764052fbcd72e
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-11-13 15:11:21 +0100

    Add env.js file

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+import { PORT, NODE_ENV } from './config/env';
+
+app.get('/', (req, res) => {
+    res.send("hello welcome");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`subscription API is running in ${NODE_ENV} mode on http://localhost:${PORT}`);
+});
+
+export default app;
